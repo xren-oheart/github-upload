de770f0e9ebda61c238e804b7ad20bd9b47df075
Xiaoli Ren <XRen@ottawaheart.ca>
2020-12-04 10:39:23 -0500

    initializing repository

---
+++ src/MoveToGit/MoveAction.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package MoveToGit;
+
+/**
+ * @author XRen
+ *
+ */
+public class MoveAction {
+
+	/**
+	 * 
+	 */
+	public MoveAction() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
